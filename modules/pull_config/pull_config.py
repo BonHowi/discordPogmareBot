ad04f3aefb98416fc2520f22f95c06858726dffb
bon <52290046+BonHowi@users.noreply.github.com>
2021-09-12 17:53:59 +0200

    Update pull_config.py

--- modules/pull_config/pull_config.py
+++ modules/pull_config/pull_config.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import pandas as pd
 import os.path
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 import json
