8bcb9c373453d002984131b623545a92c92dfd15
bon <52290046+BonHowi@users.noreply.github.com>
2021-09-13 04:37:35 +0200

    Update databasecog.py

--- cogs/databasecog.py
+++ cogs/databasecog.py
@@ -54,7 +54,7 @@ class DatabaseCog(cogbase.BaseCog):
     password = get_settings("DB_P")
     engine = create_engine(f"mysql+mysqldb://{user}:{password}@localhost/server_database?charset=utf8mb4")
     metadata_obj.create_all(engine)
-    conn = engine.connect()
+    conn = None
 
     def __init__(self, base):
         super().__init__(base)
@@ -62,13 +62,14 @@ class DatabaseCog(cogbase.BaseCog):
         # Connect to database
         self.engine = DatabaseCog.engine
         metadata_obj.create_all(self.engine)
-        self.conn = DatabaseCog.conn
         self.db_update_loop.start()
+        self.conn = DatabaseCog.conn
 
     # ----- BASE DATABASE OPERATIONS -----
 
     # Add or update member in member table
     def db_add_update_member(self, _member):
+        self.conn = self.engine.connect()
         stmt = insert(member).values(
             id=_member.id, name=_member.name,
             display_name=_member.display_name)
@@ -76,16 +77,20 @@ class DatabaseCog(cogbase.BaseCog):
             name=stmt.inserted.name, display_name=stmt.inserted.display_name
         )
         self.conn.execute(do_update_stmt)
+        self.conn.close()
 
     # Add or update spots in spots table
     def db_add_update_spots(self, spots_table, guild_member):
+        self.conn = self.engine.connect()
         stmt = insert(spots_table).values(
             member_id=guild_member.id)
         do_update_stmt = stmt.on_duplicate_key_update(member_id=stmt.inserted.member_id)
         self.conn.execute(do_update_stmt)
+        self.conn.close()
 
     # Add or refresh all guild members and spots to database
     async def db_update(self):
+        self.conn = self.engine.connect()
         guild = self.bot.get_guild(self.bot.guild[0])
         dt_string = self.get_current_time()
         print(f"({dt_string})\t[{self.__class__.__name__}]: Refreshing member and spots tables")
@@ -97,6 +102,7 @@ class DatabaseCog(cogbase.BaseCog):
             self.db_add_update_spots(spots_temp, guild_member)
         dt_string = self.get_current_time()
         print(f"({dt_string})\t[{self.__class__.__name__}]: Member and spots tables refreshed")
+        self.conn.close()
 
     @tasks.loop(hours=12)
     async def db_update_loop(self):
@@ -120,6 +126,7 @@ class DatabaseCog(cogbase.BaseCog):
                        description="Change N-Word channel name",
                        permissions=cogbase.PERMISSION_ADMINS)
     async def db_update_spots_old(self, ctx):
+        self.conn = self.engine.connect()
         import json
         with open('server_files/old_base_test.json', 'r', encoding='utf-8-sig') as fp:
             old_db = json.load(fp)
@@ -152,12 +159,14 @@ class DatabaseCog(cogbase.BaseCog):
         await ctx.send(f"Spot tables updated with old data", delete_after=3.0)
         dt_string = self.get_current_time()
         print(f'({dt_string})\t[{self.__class__.__name__}]: Spot tables updated with old data')
+        self.conn.close()
 
     # ----- SPOTTING OPERATIONS -----
 
     # Update spots tables
     @classmethod
     async def db_count_spot(cls, _id: int, monster_type: str):
+        cls.conn = cls.engine.connect()
         # Get member nr of spots for certain monster type
         stmt = select(spots.c.member_id, spots.c.legendary, spots.c.rare, spots.c.common).where(
             spots.c.member_id == _id)
@@ -175,27 +184,35 @@ class DatabaseCog(cogbase.BaseCog):
             counter = nr_of_kills[0]
         stmt = update(spots_temp).where(spots_temp.c.member_id == _id).values({f"{monster_type}": counter + 1})
         cls.conn.execute(stmt)
+        cls.conn.close()
 
     # Save coords from spotting channels to database
     @classmethod
     async def db_save_coords(cls, _coords: str, _monster_type):
+        cls.conn = cls.engine.connect()
         stmt = insert(coords).values(coords=_coords, monster_type=_monster_type)
         cls.conn.execute(stmt)
+        cls.conn.close()
 
     # Clear data from spots_temp table(for events etc)
     @classmethod
     async def db_clear_spots_temp_table(cls):
+        cls.conn = cls.engine.connect()
         stmt = delete(spots_temp)
         cls.conn.execute(stmt)
+        cls.conn.close()
 
     # ----- LEADERBOARD OPERATIONS -----
 
     # Return all members' spots
     @classmethod
     async def db_get_spots_df(cls):
+        cls.conn = cls.engine.connect()
         stmt = select(spots.c.member_id, member.c.display_name, spots.c.legendary, spots.c.rare,
                       spots.c.common).select_from(member).join(spots, member.c.id == spots.c.member_id)
+        cls.conn.execute(stmt)
         df = pd.read_sql(stmt, cls.conn)
+        cls.conn.close()
         return df
 
     # ----- WARN OPERATIONS -----
@@ -203,12 +220,15 @@ class DatabaseCog(cogbase.BaseCog):
     # Add member's warn to database
     @classmethod
     async def db_add_warn(cls, _member: int, _reason: str):
+        cls.conn = cls.engine.connect()
         stmt = insert(warn).values(member_id=_member, reason=_reason, date=datetime.now())
         cls.conn.execute(stmt)
+        cls.conn.close()
 
     # Get member's warns from database
     @classmethod
     async def db_get_warns(cls, _member: int):
+        cls.conn = cls.engine.connect()
         stmt = select(warn.c.reason, warn.c.date).select_from(member).join(warn, member.c.id == warn.c.member_id).where(
             member.c.id == _member)
         result = cls.conn.execute(stmt)
@@ -219,23 +239,28 @@ class DatabaseCog(cogbase.BaseCog):
             date_warn.append(reason_with_date)
             counter += 1
         warns_list = [': \t'.join([str(elem) for elem in sublist]) for sublist in date_warn]
+        cls.conn.close()
         return warns_list, counter
 
     # Remove all member's warns
     @classmethod
     async def db_remove_warns(cls, _member: int):
+        cls.conn = cls.engine.connect()
         stmt = delete(warn).where(warn.c.member_id == _member)
         cls.conn.execute(stmt)
+        cls.conn.close()
 
     # ----- MEMBER OPERATIONS -----
 
     # Return member's spots
     @classmethod
     async def db_get_member_stats(cls, _member: int):
+        cls.conn = cls.engine.connect()
         stmt = select(member.c.display_name, spots.c.legendary, spots.c.rare, spots.c.common).select_from(member).join(
             spots,
             member.c.id == spots.c.member_id).where(spots.c.member_id == _member)
         df = pd.read_sql(stmt, cls.conn)
+        cls.conn.close()
         return df
 
 
